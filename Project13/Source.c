ca81e947d07efca033dd767c04b6fa5c95494008
goretante <gospodingoreta@gmail.com>
2024-10-08 17:29:32 +0200

    zadatak

--- Project13/Source.c
+++ Project13/Source.c
@@ -1,8 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#define ERROR_OPENING_FILE 1
+#define BUFFER_SIZE 1024
+
+int count_rows(char* filename);
 
 int main()
 {
 	printf("Hello world!\n");
 
 	return 0;
+}
+
+int count_rows(char* filename)
+{
+	FILE* fp = NULL;
+	char buffer[BUFFER_SIZE];
+	int row_count = 0;
+
+	fp = fopen(filename, "r");
+
+	if (fp == NULL) {
+		printf("Error opening file!");
+		return ERROR_OPENING_FILE;
+	}
+
+	while (!feof(fp)) {
+		fgets(buffer, BUFFER_SIZE, fp);
+		// sscanf() - domaci rad
+		++row_count;
+	}
+
+	fclose(fp);
+
+	return row_count;
 }
